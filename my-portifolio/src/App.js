c2a351ff2944c07e57711f28978cc2a3a55333d2
kauanzeraaa <cc22316@g.unicamp.br>
2024-01-09 22:50:32 -0300

    navigatior menu

--- my-portifolio/src/App.js
+++ my-portifolio/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './components/header/Header.jsx';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+      <>
+      <Header />
+      </>
   );
 }
 
